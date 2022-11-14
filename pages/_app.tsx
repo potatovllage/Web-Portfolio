88df524adeddbeb50460bea035c370c2a258785c
Yeong joon <bluehome8626@naver.com>
2022-11-14 09:09:19 +0900

    chore: project setting

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from "next/app";
+import { Global } from "@emotion/react";
+import global from "../styles/global";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Global styles={global} />
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ styles/global.ts
@@ -0,0 +1,16 @@
+import { css } from "@emotion/react";
+
+const global = css`
+  @import url("https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap");
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    border: 0;
+    background-color: #e7edf3;
+    font-family: "noto-sans-cjk-kr", sans-serif;
+  }
+`;
+
+export default global;
