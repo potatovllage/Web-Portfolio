83988168cdcbf7511caca777a05125433661cab2
Yeong joon <bluehome8626@naver.com>
2022-11-14 17:20:53 +0900

    feat: flexWrapper 세팅

---
+++ components/common/FlexWrapper.tsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const FlexWrapper = ({ children }: Props) => {
+  return <Wrapper>{children}</Wrapper>;
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 64px;
+`;
+
+export default FlexWrapper;
