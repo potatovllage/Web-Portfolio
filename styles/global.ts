210478452e6b9e6357ff20192acd8ea3666d6cc9
Yeong joon <bluehome8626@naver.com>
2022-11-14 17:21:06 +0900

    feat: globa style 추가

--- styles/global.ts
+++ styles/global.ts
@@ -1,7 +1,20 @@
 import { css } from "@emotion/react";
 
 const global = css`
-  @import url("https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap");
+  @font-face {
+    font-family: "Pretendard-Regular";
+    src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
+      format("woff");
+    font-weight: 400;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: "Pretendard-Regular";
+    src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
+      format("woff");
+    font-weight: 700;
+    font-style: normal;
+  }
   * {
     margin: 0;
     padding: 0;
@@ -9,7 +22,10 @@ const global = css`
     box-sizing: border-box;
     border: 0;
     background-color: #e7edf3;
-    font-family: "noto-sans-cjk-kr", sans-serif;
+    font-family: "Pretendard Variable", Pretendard, -apple-system,
+      BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
+      "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic",
+      "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
 `;
 
